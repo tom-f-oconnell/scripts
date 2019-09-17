bd46a97a364bae27e5b40af3088d64bccdd3f140
Tom O'Connell <toconnel@caltech.edu>
2019-09-17 16:00:11 -0700

    Script to change git remote auth types, to be wrapped by bash aliases.

---
+++ change_git_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+Takes piped output of git remote -v and returns one line with remote changed
+to desired authentication type.
+"""
+
+import sys
+
+if __name__ == '__main__':
+    valid_auth_types = ('g','h','s')
+    if len(sys.argv) != 2:
+        raise ValueError('one required argument (g/h/s) to specify auth type')
+
+    target_auth_type = sys.argv[1]
+    assert target_auth_type in valid_auth_types, \
+        'valid auth types are {}'.format(valid_auth_types)
+
+    # TODO in general, can i get a sequence of pipes (with this in the middle)
+    # to stop early somehow (e.g. if this script only cares about the first
+    # line)
+    prefix2auth_type = {
+        'git@': 's',
+        'https://': 'h',
+        'git://': 'g'
+    }
+    for line in sys.stdin:
+        url = line.split()[1]
+        for prefix, auth_type in prefix2auth_type.items():
+            if url.startswith(prefix):
+                curr_prefix = prefix
+                current_auth_type = auth_type
+
+            if auth_type == target_auth_type:
+                target_prefix =  prefix
+
+        if current_auth_type == auth_type:
+            break
+
+        if current_auth_type == 's':
+            url = url.replace(':', '/')
+
+        sys.stdout.write(url.replace(curr_prefix, target_prefix))
+        break
+
