7d1667ccd0b2e3acae1fccdddc3fb1abefaf88b0
Tom O'Connell <toconnel@caltech.edu>
2019-11-28 11:25:16 -0600

    Python script to wrap wget for conventient bash alias.

---
+++ dlwebsite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+from urllib.parse import urlparse
+import subprocess as sp
+
+
+url = sys.argv[1]
+parts = urlparse(url)
+domain = parts.netloc
+cmd = ('wget --recursive --no-clobber --page-requisites --html-extension '
+    f'--convert-links --restrict-file-names=windows --domains {domain} '
+    f'--no-parent {url}'
+)
+sp.Popen(cmd)
+
