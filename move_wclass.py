a4afffa8f4c8b10b893bbe47b9081e881acca481
Tom O'Connell <toconnel@caltech.edu>
2021-08-23 02:03:24 -0700

    Add logic to detect which screen is on left/right if either of those strings are used as the second argument in place of a monitor name

--- move_wclass.py
+++ move_wclass.py
@@ -8,8 +8,10 @@ sudo apt-get install xdotool wmctrl
 Usage:
 move_wclass.py <WM_CLASS> <targeted_screen>
 
-Example:
+Examples:
 move_wclass.py gnome-terminal VGA-1
+move_wclass.py gnome-terminal left
+move_wclass.py gnome-terminal right
 
 In the example above, VGA-1 was taken from xrandr output like:
 VGA-1 connected 1280x1024+1680+0
@@ -19,8 +21,6 @@ import subprocess
 import sys
 
 
-# TODO TODO modify to automatically figure out which monitor is on right / left, and
-# refer to them that way?
 # TODO also add option to tile window to a certain position after moving it to a certain
 # screen?
 
@@ -34,25 +34,79 @@ def get_class(classname):
     return [w for w in w_list if classname in get(["xprop", "-id", w])]
 
 
+def is_resolution_and_offset_str(x):
+    """Returns True for strings like '1920x1080+1920+0'
+    """
+    if x.count('x') == 1 and x.count('+') == 2:
+        return True
+    return False
+
+
+def get_resolution_and_offset_str(xrandr_line_parts):
+    for x in xrandr_line_parts:
+        if is_resolution_and_offset_str(x):
+            return x
+
+    raise ValueError('no resolution string found')
+
+
+
+def side2screen_name(side, split_xrandr_lines):
+
+    if len(split_xrandr_lines) != 2:
+        raise NotImplementedError('only support side2screen name w/ two monitors')
+
+    if side not in ('left', 'right'):
+        raise ValueError("side must be either 'left' or 'right'")
+
+    if side == 'left':
+        target_offset_zero_coords = (True, True)
+
+    elif side == 'right':
+        target_offset_zero_coords = (False, True)
+
+    for xrandr_line_parts in split_xrandr_lines:
+        res_and_offset_str = get_resolution_and_offset_str(xrandr_line_parts)
+        x_offset, y_offset = [int(x) for x in res_and_offset_str.split('+')[1:]]
+
+        offset_zero_coords = (x_offset == 0, y_offset == 0)
+
+        if offset_zero_coords == target_offset_zero_coords:
+            name = xrandr_line_parts[0]
+            return name
+
+    raise ValueError("no monitor seemed to match side '{}'".format(side))
+
+
 def main():
     # get the data on all currently connected screens, their x-resolution
     screendata = [l.split() for l in get(["xrandr"]).splitlines() if " connected" in l]
-    screendata = sum(
+
+    screen_name = sys.argv[2]
+
+    # Otherwise, `screen_name` should match the first part in one of the lines in
+    # `screendata` exactly (from `xrandr` output).
+    if screen_name in ('left', 'right'):
+        side = screen_name
+        screen_name = side2screen_name(side, screendata)
+
+    # In format like: [[('HDMI-2', '1920')], [('DP-1', '0')]]
+    screennames_and_x_offsets = sum(
         [[(w[0], s.split("+")[-2]) for s in w if s.count("+") == 2]
          for w in screendata
         ],
         []
     )
 
-    scr = sys.argv[2]
-
     try:
         # determine the left position of the targeted screen (x)
-        pos = [sc for sc in screendata if sc[0] == scr][0]
+        pos = [sc for sc in screennames_and_x_offsets if sc[0] == screen_name][0]
+
     except IndexError:
         # TODO list valid screen IDs that are connected in this case
         # warning if the screen's name is incorrect (does not exist)
-        print(scr, "does not exist. Check the screen name")
+        print(screen_name, "does not exist. Check the screen name")
+
     else:
         # TODO error message if window class is wrong / instructions for finding it
         for w in get_class(sys.argv[1]):
